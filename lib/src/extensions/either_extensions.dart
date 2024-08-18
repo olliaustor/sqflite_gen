3e80ac8e635cefa2d9520e5ebd935fe6f0ee529f
Oliver Saß <oliver.sass@safetyio.com>
2024-08-18 14:46:51 +0200

    Initial commit for example app

--- lib/src/extensions/either_extensions.dart
+++ lib/src/extensions/either_extensions.dart
@@ -1,6 +1,6 @@
 import 'package:fpdart/fpdart.dart';
 
 extension EitherX<L, R> on Either<L, R> {
-  R asRight() => (this as Right).value; //
-  L asLeft() => (this as Left).value;
+  R asRight() => (this as Right).value as R; //
+  L asLeft() => (this as Left).value as L;
 }
