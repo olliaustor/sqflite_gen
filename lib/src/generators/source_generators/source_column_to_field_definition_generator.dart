4b0869ec9f3068bb776354354be3209c55580b8e
Oliver Saß <oliver.sass@safetyio.com>
2024-09-22 20:50:02 +0200

    Deleted unused files

--- lib/src/generators/source_generators/source_column_to_field_definition_generator.dart
+++ lib/src/generators/source_generators/source_column_to_field_definition_generator.dart
@@ -1,4 +1,3 @@
-import 'package:sqflite_gen/src/converters/column_name_to_const_name_converter.dart';
 import 'package:sqflite_gen/src/converters/underscore_to_camel_case_converter.dart';
 import 'package:sqflite_gen/src/generators/source_generators/source_generator_base.dart';
 import 'package:sqlparser/sqlparser.dart';
