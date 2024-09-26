bae04b18bc538499ec114f94623cf54c716e3cbc
Oliver Saß <oliver.sass@safetyio.com>
2024-09-26 19:46:21 +0200

    manually changed generated file as correct blueprint

--- example/lib/db/database.dart
+++ example/lib/db/database.dart
@@ -1,6 +1,18 @@
-import 'generic_provider.dart';
+import 'package:example_app/db/tables/car_options/car_options_values.dart';
+import 'package:example_app/db/tables/car_parts/car_parts_values.dart';
+import 'package:example_app/db/tables/car_vins/car_vins_values.dart';
+import 'package:example_app/db/tables/customer_ownership/customer_ownership_values.dart';
+import 'package:example_app/db/tables/customers/customers_values.dart';
+import 'package:example_app/db/tables/dealer_brand/dealer_brand_values.dart';
+import 'package:example_app/db/tables/dealers/dealers_values.dart';
+import 'package:example_app/db/tables/manufacture_plant/manufacture_plant_values.dart';
+import 'package:example_app/db/tables/models/models_values.dart';
+
+import 'tables/brands/brands_values.dart';
 import 'tables/tables.dart';
 import 'package:sqflite/sqflite.dart';
+
+typedef GetTableProvider = List<String> Function(int);
 
 Future<Database> openDatabaseWithMigration(String path) async {
   return openDatabase(path,
@@ -14,7 +26,7 @@ Future<void> _onCreate(Database db, int version) async {
   final tables = _getTableProviders(db);
 
   for (final table in tables) {
-    scriptList.addAll(table.create(version));
+    scriptList.addAll(table(version));
   }
 
   final batch = db.batch();
@@ -24,18 +36,18 @@ Future<void> _onCreate(Database db, int version) async {
   await batch.commit(noResult: true);
 }
 
-List<GenericProvider<Object>> _getTableProviders(Database db) {
+List<GetTableProvider> _getTableProviders(Database db) {
   return [
-    CustomersProvider(db),
-    CarVinsProvider(db),
-    CarOptionsProvider(db),
-    CarPartsProvider(db),
-    BrandsProvider(db),
-    ModelsProvider(db),
-    CustomerOwnershipProvider(db),
-    ManufacturePlantProvider(db),
-    DealersProvider(db),
-    DealerBrandProvider(db),
+    (int version) => [customersTableCreate],
+    (int version) => [carVinsTableCreate],
+    (int version) => [carOptionsTableCreate],
+    (int version) => [carPartsTableCreate],
+    (int version) => [brandsTableCreate],
+    (int version) => [modelsTableCreate],
+    (int version) => [customerOwnershipTableCreate],
+    (int version) => [manufacturePlantTableCreate],
+    (int version) => [dealersTableCreate],
+    (int version) => [dealerBrandTableCreate],
   ];
 }
 
