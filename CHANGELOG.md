da1b106794c3b99cb25bf6b495e3ffef1de41722
Oliver Sass <03sasso42@msasafety.com>
2025-05-04 13:21:20 +0200

    Bumped packages to latest versions and increased pub.dev score

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
+# 0.1.1
+- Bumps packages to latest versions
+- Fixes issues to increase pub.dev score
+
 # 0.1.0
 
 Initial version of the library.
 
 - Includes database creation
 - Includes generation of basic access to the database (table specific provider classes)
-- Includes generation of table specific model classes
+- Includes generation of table-specific model classes
--- lib/src/generators/file_generators/table_model/source_generator/to_map/column_int_to_assignment_generator.dart
+++ lib/src/generators/file_generators/table_model/source_generator/to_map/column_int_to_assignment_generator.dart
@@ -1,10 +1,8 @@
-import 'dart:ffi';
-
 import 'package:sqflite_gen/src/extensions/column_definition_extensions.dart';
 import 'package:sqflite_gen/src/generators/file_generators/table_model/source_generator/to_map/column_assignment_generator.dart';
 import 'package:sqlparser/sqlparser.dart';
 
-/// Generates map value assignment for [Int] value
+/// Generates map value assignment for [int] value
 class ColumnIntToAssignmentGenerator implements ColumnToAssignmentGenerator {
   /// Generates assignment for given [ColumnDefinition]
   ///  columnDefinition': Column definition
